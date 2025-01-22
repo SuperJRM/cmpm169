abdc83e40af454c921554c0f1f4a2ce5c1ae5b44
SuperJRM <163477766+SuperJRM@users.noreply.github.com>
2025-01-22 00:38:26 -0800

    Experiment 2 Upload

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -49,6 +49,28 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+
+  // Start of setup() code
+  angleMode(DEGREES);
+  bronze = color(208,162,52)
+  // Array storing current rotations of gears
+  spinY = []
+  for (let sy = 0; sy < 5; sy++) {
+    let spinX = [];
+    for (let sx = 0; sx < 5; sx++) {
+      spinX.push(0);
+    }
+    spinY.push(spinX);
+  }
+  // Array storing current acceleration of gears
+  inY = []
+  for (let sy = 0; sy < 5; sy++) {
+    let inX = [];
+    for (let sx = 0; sx < 5; sx++) {
+      inX.push(0);
+    }
+    inY.push(inX);
+  }
 }
 
 // draw() function is called repeatedly, it's the main animation loop
@@ -71,9 +93,87 @@ function draw() {
   textStyle(BOLD);
   textSize(140);
   text("p5*", centerHorz - 105, centerVert + 40);
+
+  // Start of draw() code
+  background(0);
+  // Creates 5x5 grid of gears
+  for (let sy = 0; sy < 5; sy++) {
+    for (let sx = 0; sx < 5; sx++) {
+      push()
+      scale(.5)
+      translate(sx*500, sy*500);
+      // Uses rotation and acceleration from array
+      spinCount = spinY[sy][sx];
+      spIncrease = inY[sy][sx];
+      // Rotates canvas and creates rotated gear
+      spinCount = spinCount + spIncrease;
+      rotate(spinCount);
+      gearMake()
+      // Decreases acceleration
+      if(spIncrease>0){
+        spIncrease-= 1.75/deltaTime;
+      }
+      if (spIncrease<0) {
+        spIncrease=0;
+      }
+      // Saves new rotation and acceleration to array
+      spinY[sy][sx] = spinCount;
+      inY[sy][sx] = spIncrease;
+      pop();
+    }
+  }
+}
+
+// Function to draw a gear
+function gearMake() {
+  noFill();
+  circle(0,0,100);
+  fill(bronze);
+  circle(0,0,250);
+  fill(0,0,0)
+  circle(0,0,100);
+  // For loop draws gear teeth
+  for (let angle=0; angle<=330; angle+=30) {
+    push();
+    rotate(angle);
+    fill(bronze);
+    noStroke();
+    square(-18,-155,36);
+    pop();
+  }
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  gearX = round(mouseX/250);
+  gearY = round(mouseY/250);
+  print(gearX,gearY)
+  inY[gearY][gearX] += 3.5;
+  
+  // Edge Cases and neighbor gear acceleration
+  if (gearX != 0 && gearY != 0) {
+    inY[gearY-1][gearX-1] += 1.75;
+  }
+  if (gearX != 0) {
+    inY[gearY][gearX-1] += 2.5;
+  }
+  if (gearY != 0) {
+    inY[gearY-1][gearX] += 2.75;
+  }
+  if (gearX != 5 && gearY != 5) {
+    inY[gearY+1][gearX+1] += 1.75;
+  }  
+  if (gearX != 5) {
+    inY[gearY][gearX+1] += 2.5;
+  }  
+  if (gearY != 5) {
+    inY[gearY+1][gearX] += 2.5;
+  }
+  
+  if (gearX != 0 && gearY != 5) {
+    inY[gearY+1][gearX-1] += 1.75;
+  }
+  if (gearX != 5 && gearY != 0) {
+    inY[gearY-1][gearX+1] += 1.75;
+  }
 }
