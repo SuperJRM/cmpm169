7aa75fc7a4f88314bb0e5740b942643531df3230
SuperJRM <163477766+SuperJRM@users.noreply.github.com>
2025-01-29 19:09:32 -0800

    Experiment 3 Complete

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -14,6 +14,10 @@ const VALUE2 = 2;
 let myInstance;
 let canvasContainer;
 var centerHorz, centerVert;
+let angle = 4;
+let count = 0;
+let balance = "both"
+let zap;
 
 class MyClass {
     constructor(param1, param2) {
@@ -24,6 +28,11 @@ class MyClass {
     myMethod() {
         // code to run when method is called
     }
+}
+
+function preload() {
+  soundFormats('mp3');
+  zap = loadSound('./sounds/zip.mp3')
 }
 
 function resizeScreen() {
@@ -71,6 +80,52 @@ function draw() {
   textStyle(BOLD);
   textSize(140);
   text("p5*", centerHorz - 105, centerVert + 40);
+
+  background(0);
+  if (mouseIsPressed) {
+    if (!zap.isPlaying()) {
+      zap.play();      
+    }
+    stroke(random(167,212), random(100,147), 255);
+    translate(width / 2, height);
+    branch(height / 4, angle, count, balance);
+  }
+  else {
+    zap.stop()
+  }
+}
+
+function branch(len, angle, count, balance) {
+  angle = PI/random(6,10)
+  line(0, 0, 0, -len);
+  translate(0, -len);
+  if (count <= 3) {
+    count++;
+    if (balance == "right" || balance == "both") {
+      push();
+      rotate(angle);
+      if (balance == "right") {
+        tempBalance = random(["left", "both", "none"]);
+      }
+      else {
+        tempBalance = random(["left, right", "both"])
+      }
+      branch(len * random(0.5, 1.2), angle, count, tempBalance);
+      pop();
+    }
+    if (balance == "left" || balance == "both") {
+      push();
+      rotate(-angle);
+      if (balance == "left") {
+        tempBalance = random(["right", "both", "none"]);
+      }
+      else {
+        tempBalance = random(["left, right", "both"])
+      }
+      branch(len * random(0.5, 1.2), angle, count, tempBalance);
+      pop();
+    }
+  }
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
